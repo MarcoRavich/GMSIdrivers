58ecb35ea22ebb6908200b9765dfe634d2f7108e
Marco Ravich <forartfe@gmail.com>
2025-11-13 07:34:43 +0000

    Add files via upload

---
+++ ISISALSA/alsa-driver-0.9.5-isis/include/amagic.h
@@ -0,0 +1,10 @@
+/*
+ * additional magic numbers for extra drivers
+ */
+#ifdef CONFIG_SND_DEBUG_MEMORY
+
+#define snd_msndpinnacle_pcm_t_magic		0xa25a3e01
+#define msndmidi_t_magic			0xa25a3e02
+#define pdacf_t_magic				0xa25a3f00
+
+#endif
---
+++ ISISALSA/alsa-driver-0.9.5-isis/include/version.h
@@ -0,0 +1,3 @@
+/* include/version.h.  Generated automatically by configure.  */
+#define CONFIG_SND_VERSION "0.9.5"
+#define CONFIG_SND_DATE ""
